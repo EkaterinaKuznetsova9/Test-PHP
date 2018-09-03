0ff99cc9e184e7cd56a0e118bc10e2599ac0b60e
EkaterinaKuznetsova9 <32989353+EkaterinaKuznetsova9@users.noreply.github.com>
2018-09-04 01:15:03 +0300

    Update 3.php

--- 3.php
+++ 3.php
@@ -1,16 +1,17 @@
- <?php
- class Figure
+<?php
+
+class Figure // класс ФИГУРЫ
     {
         protected $type = '';
         public function getArea() {}
         public function getType()
         {
-            if ($this->type == '') return 'Не определен'; //можно добавить, что площадь = 0
+            if ($this->type == '') return 'Не определен';
             else return $this->type;
         }
     }
-    
-	class Circle extends Figure
+	
+	class Circle extends Figure  //дочерний класс круги
     {
         private $radius;
         function __construct($r = 0)
@@ -21,19 +22,19 @@
             {
                 $this->type = 'Circle';
                 $this->radius = $r;   
-            }    
-                
+            }  
+			                
         } 
         public function getArea()
         {
-            if (!$this->type) return '';
-            return M_PI * $this->radius * $this->radius;
+            if (!$this->type) return '0';
+			return M_PI * $this->radius * $this->radius;
         }
         
            
     }    
 	
-    class Rectangle extends Figure
+	class Rectangle extends Figure //дочерний класс прямоугольники
     {
         private $st1, $st2;
         function __construct($st1 = 0, $st2=0)
@@ -49,70 +50,60 @@
         }
         public function getArea()
         {
-            if (!$this->type) return '';
+            if (!$this->type) return '0';
             return $this->st1 * $this->st2;
         }
             
     }
-    
-    
-    
-    class Pyramid extends Figure //у нас будут исключительно правильные пирамиды
+	
+	class Pyramid extends Figure //дочерний класс пирамиды
+	/* у нас будут они исключительно правильные, иначе не получится вывести некую общую ф-лу для нахождения площади пирамиды.
+	Но, вообще, важно, если в основании неправильный треугольник, то нужно задавать все три стороны, а также нужно добавить
+	условие на то, что сумма двух любых сторон больше третьей,	иначе площадь = 0. */
     {
-        private $num, $a, $h;
-        function __construct($num = 0, $b = 0, $h = 0)
+        private $num, $a, $apofema; //$num - количество углов/сторон у фигуры в основании, $a - длина стороны, $apofema - длина апофемы
+        function __construct($num = 0, $a = 0, $apofema = 0)
         {
             $this->type = '';
-            $b = floatval($a); $h = floatval($h);
-            if (($num > 0 ) && ($a > 0) && ($h > 0)) 
+            $b = floatval($a); $apofema = floatval($apofema);
+            if (($num > 0 ) && ($a > 0) && ($apofema > 0)) 
             {
-                $p = ($a * $num) / 2; // полупериметр
-                
+                                
                     $this->type = 'Pyramid';
                     $this->num = $num;                    
                     $this->a = $a;  
-                    $this->h = $h;  
+                    $this->apofema = $apofema;  
                 
             }    
         }
         
-        public function getArea() //площадь боковой поверхности (через периметр и апофему)
+        public function getArea() //площадь боковой поверхности (через полупериметр и апофему) + площадь основания(через тангенс)
         {
-            if (!$this->type) return '';
-            $p = ($this->a * $this->num) / 2; // полупериметр
-            return ($p * $this->h); // S = p*h
+            if (!$this->type) return '0';
+            $sBok = $this->h * $this->a * $this->num / 2; // пл-дь боковой пов-ти
+			$sOsn = $this->num * pow($this->a, 2)/tan(180/$this->num); // пл-дь основания
+            return ($sBok + $sOsn) ; 
         }
         
              
     }
-
-
-
-    $x = ''; // массив
-	//$f = [{"type":"circle","radius":8},{"type":"rectangle","st1":4,"st2":5},{"type":"Pyramid","num":3,"b":4,"h":6},{"type":"rectangle","st1":6,"st2":8}, {"type":"circle","radius":7}];
-	$f = file('file.txt');// чтение объектов из файла;
-    foreach ($f as $val)
-    {
-        $who = explode(',', trim($val)); 
-        $who[] = 0; $who[] = 0; $who[] = 0; 
-        switch ($who[0]) 
-        {
-            case 'circle':
-                $x[] = new Circle($who[1]);
-                break;
-            case 'rectangle':
-                $x[] = new Rectangle($who[1]);
-                break;
-            case 'pyramid':
-                $x[] = new Pyramidwho[1], $who[2], $who[3]);
-                break;                
-        }
-    }
-    if ($x)    
-    {
-        foreach ($x as $v)
-        {
-            echo $v->getType(). ' , Площадь=', $v->getArea();
-        }
-    }    
-?>
+	
+// ПОЛУЧЕНИЕ ОБЪЕКТОВ ИЗ ФАЙЛА	
+$path = file_get_contents("figures.json");
+$json = json_decode($path, true);
+for($i=0;$i<count($json);$i++) {
+	switch ($json[$i]['type']) {
+	    case 'circle':
+	        $figure = new Circle($json[$i]['radius']);
+			break;
+		case 'rectangle':
+            $figure = new Rectangle($json[$i]['st1'], $json[$i]['st2']);
+			break;
+		case 'pyramid':
+            $figure = new Pyramid($json[$i]['num'], $json[$i]['a'], $json[$i]['apofema']);
+			break;
+	}
+	$json[$i]['square'] = $figure->getArea();
+	echo $json[$i]['square'];
+	echo ("-----");
+}
